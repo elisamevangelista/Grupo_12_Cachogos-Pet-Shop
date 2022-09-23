16f4240d1f077b1a8913e3fa2f5afcb97f7c33a9
Andres9092 <richardsonandres1@gmail.com>
2022-09-23 08:58:53 -0300

    armado de header 23-9

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))  //se puede prescidir de este termino __dirname//
+
 
 
 app.listen(3000, ()=>{
     console.log('Servidor levantado');
 });
 
+
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
 });
