418339baa1c9048a97b425412cc6a5bbcc7b41f3
Billy Obregon <apptalentos@gmail.com>
2022-10-23 20:14:28 -0500

    creacion de pagina de ver productos

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,6 +1,46 @@
 
 // edicionProdcuto
 
+let productlist = [
+    {
+        "nombre": "PRO PLAN PERRO ADULTO MEDIANO X 15 KG + 3 KG ",
+        "categoria": "alimentos",
+        "img" : '<img src="/public/image/card-produc1.jpg">',
+        "precio" : 100.00
+    },
+    {
+        "nombre": "DOGUI CACHORROS CARNE CEREALES Y LECHE X 21 KG ",
+        "categoria": "alimentos",
+        "img" : "/image/card-produc1.jpg",
+        "precio" : 150.00
+    },
+    {
+        "nombre": "NUTRIQUE PERRO BAJAS CALORIAS X 3 KG ",
+        "categoria": "alimentos",
+        "img" : "/public/image/card-produc1.jpg",
+        "precio" : 300.00
+    },
+    {
+        "nombre": "VITALCAN COMPLETE PERRO CACHORRO RAZA MED/GRA X 3 KG ",
+        "categoria": "alimentos",
+        "img" : "/public/image/card-produc1.jpg",
+        "precio" : 800.00
+    },
+
+    {
+        "nombre": "OUTLET KONGO NATURAL CACHORROS X 21 KG ",
+        "categoria": "alimentos",
+        "img" : "/public/image/card-produc1.jpg",
+        "precio" : 100.00
+    },
+    {
+        "nombre": "COMPANY PERRO CACHORRO X 20 KG ",
+        "categoria": "alimentos",
+        "img" : "/public/image/card-produc1.jpg",
+        "precio" : 20.00
+    },
+    
+]
 
 const productControllers = {
     creacionproducto: (req, res) => {
@@ -14,6 +54,12 @@ const productControllers = {
     },
     verproducto: (req, res) => {
         res.render('verproducto')
+    },
+
+    productlist: (req, res) => {
+        res.render('productlist', {
+            'productlist': productlist
+        })
     }
  
 }
