14da2dc1b6e4ccc7141fdee3f5cba335b6eb81dd
Richardson <andres.richardson@siemens.com>
2023-01-21 18:08:16 -0300

    validacionRegister front 21/1

--- public/js/validacionReg.js
+++ public/js/validacionReg.js
@@ -1,30 +1,32 @@
-window.addEventListener('load', function(){
+window.onload = function(){
 
     let formularioRegister = document.querySelector('form.formuRegister')
 
+    
+    let arrayError =[]
+        
     formularioRegister.addEventListener('submit', function(e){
 
-
-        let errores =[]
-        
-        let nombre = document.querySelector('input.nameInput')
+        arrayError = []
+       
+        let nombre = document.querySelector('#nombre')
         if(nombre.value == ""){
-            errores.push('Este campo es obligatorio.')
+            arrayError.push('El campo nombre es obligatorio.')
         } else if (nombre.value.length < 2){
-            errores.push('El nombre debe tener al menos 2 caracteres.')
+            arrayError.push('El nombre debe tener al menos 2 caracteres.')
         }
 
         let apellido = document.querySelector('input.lastnameInput')
         if(apellido.value == ""){
-            errores.push('Este campo es obligatorio.')
-        } else if (apellido.value.length <= 2){
-            errores.push('El apellido debe tener al menos 2 caracteres.')
+            arrayError.push('El campo apellido es obligatorio.')
+        } else if (apellido.value.length < 2){
+            arrayError.push('El apellido debe tener al menos 2 caracteres.')
         }
 
         let emailForm = document.querySelector('input.mailInput')
 
         function isEmail(email){
-            return /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z](2,4))$/.test(email);
+            return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email);
         }
 
         function isPassword(passwor){
@@ -32,49 +34,70 @@ window.addEventListener('load', function(){
         }
 
         if(emailForm.value == ""){
-            errores.push('Este campo es obligatorio.')
+            arrayError.push('El campo email es obligatorio.')
         } else if (isEmail(emailForm.value) === false){
-                errores.push('Ingrese un e-mail válido.')
+                arrayError.push('Ingrese un e-mail válido.')
                 
         }
+
+
+        let allowedExtension = ['jpeg', 'jpg', 'png', 'gif']; 
+        let fileExtension = document.getElementById('image').value.split('.').pop().toLowerCase()
+       
+        if (allowedExtension.indexOf(fileExtension) == -1) {
+            arrayError.push('El formato de la imagen cargada no es válido.')  
+             
+        }
+      
+        
+        // for (let i =0; i< allowedExtension.length; i++) {
+
+
+        //     if (fileExtension === allowedExtension[i]) {
+                
+        //         break;
+        //     } else {
+        //         arrayError.push('El formato de la imagen cargada no es válido.')
+        //     }
+        // }
+
 
         let passwordForm = document.querySelector('input.contraInput')
 
         if(passwordForm.value == ""){
-            errores.push('Este campo es obligatorio.')
-        } else if (passwordForm.value.length <= 8){
-            errores.push('La contraseña debe tener al menos 8 caracteres.')
+            arrayError.push('El campo password es obligatorio.')
+        } else if (passwordForm.value.length < 8){
+            arrayError.push('La contraseña tiene que ser de 8 a 16 carácteres.')
         } else if (isPassword(passwordForm.value) === false){
-            errores.push('La contraeña no es válida.')
+            arrayError.push('La contraseña debe incluir minimamente una mayúscula, una minúscula, un número y un caracter especial.')
         }
          
-                            
+                                                            //bird-categoria.jpg--> ['bird-categoria', 'jpg' ]  -> ['JPG'] -> ['jpg']
         // function validateFile(fileExtension) {
-            let allowedExtension = ['jpeg', 'jpg', 'png', 'gif']; 
-            let fileExtension = document.getElementById('image').value.split('.').pop().toLowerCase()
-           
-            for (let i =0; i< allowedExtension.length; i++) {
+            
+            // if(arrayError.length > 0){
 
-                if (fileExtension === allowedExtension[i]) {
-                    
-                    break;
-                } else {
-                    errores.push('El formato de la imagen cargada no es válido.')
-                }
-            }
+            //     e.preventDefault()
 
-            if(errores.length > 0){
+            //     let ularrayError = document.querySelector('div.arrayError ul')  //etiqueta UL dentro del div clase 'arrayError' creado en archivo register.
 
+            //     for(let i =0; i < arrayError.length; i++){
+
+            //         ularrayError.innerHTML += "<li>" + arrayError[i] + "</li>"
+            //     }
+            // }
+            console.log('arrayError:', arrayError)
+
+            let errores = document.querySelector('.errores')
+
+            if (arrayError.length > 0) {
                 e.preventDefault()
-
-                let ulErrores = document.querySelector('div.errores ul')  //etiqueta UL dentro del div clase 'errores' creado en archivo register.
-
-                for(let i =0; i < errores.length; i++){
-
-                    ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+                errores.innerHTML = ''
+                for (let error of arrayError) {
+                    errores.innerHTML += `<li>${error}</li>`
                 }
             }
     
         })
 
-})
+}
