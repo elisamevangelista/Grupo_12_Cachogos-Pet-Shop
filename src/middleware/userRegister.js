34f6fb31bb102fccb8abbfe617401d1579b642e9
elisamevangelista <elisamevangelista@yahoo.com.ar>
2023-01-26 21:33:14 -0300

    validación back 26-01

--- src/middleware/userRegister.js
+++ src/middleware/userRegister.js
@@ -1,71 +1,92 @@
-const { check, body } = require("express-validator");       //Middleware utilizado para verificar que los datos ingresados por el usuario al registrarse en el formulario, tengan el formato correcto esperado.
-const usersControllers = require("../controllers/usersControllers");
+// const { check, body } = require("express-validator");       //Middleware utilizado para verificar que los datos ingresados por el usuario al registrarse en el formulario, tengan el formato correcto esperado.
+// const usersControllers = require("../controllers/usersControllers");
+// const db = require('../db/models');
+
+// let validationRegister 
+
+// db.Users.findAll()
+//     .then((users) => {
+
+//     validationRegister = 
+//     [
+//         check("nombre")
+//         .exists()
+//         .withMessage("El campo de nombre es obligatorio")
+//         .bail()
+//         .isLength({ min:2 })
+//         .withMessage("El campo debe contar al menos con dos caracteres"),
+
+//         check("apellido")
+//         .exists()
+//         .withMessage("El campo de apellido es obligatorio")
+//         .bail()
+//         .isLength({ min:2 })
+//         .withMessage("el campo debe contar al menos con dos caracteres"),
+
+//         check("email")
+//         .custom(async function (value){
+//             let contador = 0;
+//             for (let i=0; i < users.length; i++) {
+
+//                 if(users[i].email == value) {
+
+//                     contador = contador + 1
+
+//                 }
+//             }
+//             console.log("contador:", contador)
+//                 if (contador == 0){
+
+//                     return false;
 
 
-let validationRegister = 
-    [
-        check("nombre").isLength({
-            min:2
-        })
-        .withMessage("el nombre es obligatorio y debe contar al menos con dos caracteres"),
-        check("apellido").isLength({
-            min:2
-        })
-        .withMessage("el apellido es obligatorio y debe contar al menos con dos caracteres"),
-        check("email").exists().withMessage("El campo de Email es obligatorio"),
-        check("email").isEmail().withMessage("Formato de Email no es válido"),
-        body('email').custom(function (value){
+//                 } else {
 
-            let contador = 0;
-            for (let i=0; i < users.length; i++) {
+//                     return true;
+//                 }
 
-                if(users[i].email == value) {
+//             })
+//         .withMessage('El usuario ya existe. Por favor, ingrese otro e-mail diferente.')
+//         .bail() //es como un .next, si pasa la primera validación pasa a analizar la segunda
+//         .isEmail()
+//         .withMessage("Formato de Email no es válido"),
+                     
+//         check("password")
+//         .exists()
+//         .withMessage("El password es obligatorio")
+//         .bail()
+//         .isLength({ min: 8 })
+//         .withMessage("La contraseña debe contener al menos 8 caracteres")
+//         .bail()
+//         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/gm)   // el  *\d cheque que al menos haya un numero/digito ingresado.
+//         .withMessage('La contraseña debe incluir minimamente una mayúscula, una minúscula, un número y un caracter especial'),
+        
+//         body('image')
+//         .custom(function (value, {req}){
 
-                    contador = contador + 1
+//             let ext
+//             if(req.file != undefined){
 
-                }
-            }
-                if (contador == 0){
+//                 ext = "" +path.extname(req.file.filename).toLowerCase() 
 
-                    return true;
-
-
-                } else{
-
-                    return false;
-                }
-
-            }).withMessage('El usuario ya existe. Por favor, ingrese otro e-mail diferente.'), 
-            
-            
-        check("password").exists().withMessage("La contraseña debe contener al menos 8 caracteres"),
-        check("password").isLength({ min: 8 }).withMessage("La contraseña debe contener al menos 8 caracteres"),
-        check("password").matches(/^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/gm),   // el  *\d cheque que al menos haya un numero/digito ingresado.
-        //.withMessage('Obligatoriamente debe tener una mayuscula y un numero minimamente'),
-        body('image').custom(function (value, {req}){
-
-            let ext
-            if(req.file != undefined){
-
-                ext = "" +path.extname(req.file.filename).toLowerCase() 
-
-                if (
-                    ext == ".jpg" ||
-                    ext == ".jpeg" ||
-                    ext == ".png"   ||
-                    ext == ".gif" ) {
+//                 if (
+//                     ext == ".jpg" ||
+//                     ext == ".jpeg" ||
+//                     ext == ".png"   ||
+//                     ext == ".gif" ) {
     
-                        return true;
-                    } else {
-                        return false
-                    }
-            } else {
-                return false
-            }         
+//                         return true;
+//                     } else {
+//                         return false
+//                     }
+//             } else {
+//                 return false
+//             }         
 
-        }).withMessage('Archivo no cargado o no compatible con las siguientes extensiones: JPG, JPEG, PING O GIF.'), 
+//         })
+//         .withMessage('Archivo no cargado o no compatible con las siguientes extensiones: JPG, JPEG, PING O GIF.'), 
 
-    ]
-    
+//     ]
+// })   
 
-module.exports = validationRegister
+// module.exports = validationRegister
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { check } = require("express-validator")  
+const { check, body } = require("express-validator")  
 
-const validacionRegister = require('../middleware/userRegister')
+//const validacionRegister = require('../middleware/userRegister')
 const validacionLogin = require('../middleware/userLogin')
 const multer = require('multer');
 
@@ -31,7 +31,90 @@ router.get('/register', userLogged, usersController.register)  // image es el va
 db.Users.findAll()
     .then((users) => {
         
-        router.post('/register', uploadFile.single('image'), validacionRegister, usersController.store); //validacionRegister-> SERIA EL MIDDLEWARE. LO SACAMOS PORQUE DEVUELVE ERROR
+        router.post('/register', uploadFile.single('image'), 
+        [
+            check("nombre")
+            .exists()
+            .withMessage("El campo de nombre es obligatorio")
+            .bail()
+            .isLength({ min:2 })
+            .withMessage("El campo debe contar al menos con dos caracteres"),
+    
+            check("apellido")
+            .exists()
+            .withMessage("El campo de apellido es obligatorio")
+            .bail()
+            .isLength({ min:2 })
+            .withMessage("el campo debe contar al menos con dos caracteres"),
+    
+            check("email")
+            .custom(async function (value){
+                console.log("value:", value)
+                if(typeof value == '') {
+                    return Promise.reject('El email es obligatorio');
+                }
+                let contador = 0;
+                for (let i=0; i < users.length; i++) {
+    
+                    if(users[i].email == value) {
+    
+                        contador = contador + 1
+    
+                    }
+                }
+                //console.log("contador:", contador)
+                    if (contador == 0){
+    
+                        return true;
+    
+    
+                    } else {
+    
+                        return false;
+                    }
+    
+                })
+            .withMessage('El usuario ya existe. Por favor, ingrese otro e-mail diferente.')
+            .bail() //es como un .next, si pasa la primera validación pasa a analizar la segunda
+            .isEmail()
+            .withMessage("Formato de Email no es válido"),
+                         
+            check("password")
+            .exists()
+            .withMessage("El password es obligatorio")
+            .bail()
+            .isLength({ min: 8 })
+            .withMessage("La contraseña debe contener al menos 8 caracteres")
+            .bail()
+            .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/gm)   // el  *\d cheque que al menos haya un numero/digito ingresado.
+            .withMessage('La contraseña debe incluir minimamente una mayúscula, una minúscula, un número y un caracter especial'),
+            
+            body('image')
+            .custom(function (value, {req}){
+    
+                let ext
+                if(req.file != undefined){
+    
+                    ext = "" +path.extname(req.file.filename).toLowerCase() 
+    
+                    if (
+                        ext == ".jpg" ||
+                        ext == ".jpeg" ||
+                        ext == ".png"   ||
+                        ext == ".gif" ) {
+        
+                            return true;
+                        } else {
+                            return false
+                        }
+                } else {
+                    return false
+                }         
+    
+            })
+            .withMessage('Archivo no cargado o no compatible con las siguientes extensiones: JPG, JPEG, PING O GIF.'), 
+    
+        ], usersController.store); //validacionRegister-> SERIA EL MIDDLEWARE. LO SACAMOS PORQUE DEVUELVE ERROR
 
     })
     .catch((errors) => {
